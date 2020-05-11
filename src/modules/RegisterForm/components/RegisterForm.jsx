871a78b6b147acd6140e913c8c48fef10aa68af3
Gleb <gleb4ist@gmail.com>
2020-05-12 02:14:58 +0300

    валидация формы регистрации

--- src/modules/RegisterForm/components/RegisterForm.jsx
+++ src/modules/RegisterForm/components/RegisterForm.jsx
@@ -1,79 +1,110 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Form, Input } from 'antd'
 import { Button, Block } from 'components'
 import { UserOutlined, LockOutlined, MailOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom'
 
-class RegisterForm extends Component {
-    render() {
-        const success = false
-        return (
-            <>
-                <div className="auth__top">
-                    <h2>Регистрация</h2>
-                    <p>Для входа в чат, вам нужно зарегистрироваться</p>
-                </div>
-                <Block>
-                    { !success ? (
-                        <Form
-                            name="normal_login"
-                            className="login-form"
-                            initialValues={{ remember: true }}
-                            onFinish={this.onFinish}
+
+const success = false
+
+const RegisterForm = props => {
+    const {
+        touched,
+        errors,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+    } = props;
+    return (
+        <>
+            <div className="auth__top">
+                <h2>Регистрация</h2>
+                <p>Для входа в чат, вам нужно зарегистрироваться</p>
+            </div>
+            <Block>
+                {!success ? (
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        initialValues={{ remember: true }}
+                    >
+                        <Form.Item
+                            hasFeedback
+                            name="email"
+                            validateStatus={
+                                !touched.normal_login_email ? "" : errors.email ? "error" : "success"
+                            }
+                            help={!touched.normal_login_email ? false : errors.email}
                         >
-                            <Form.Item
-                                hasFeedback
-                                name="email"
-                            >
-                                <Input prefix={<MailOutlined className="site-form-item-icon" />} placeholder="E-mail" size="large" />
-                            </Form.Item>
-                            <Form.Item
-                                name="nickname"
-                            >
-                                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Ваше имя" size="large" />
-                            </Form.Item>
-                            <Form.Item
-                                hasFeedback
-                                name="password"
-                            >
-                                <Input
-                                    type="password"
-                                    prefix={<LockOutlined className="site-form-item-icon" />}
-                                    placeholder="Пароль"
-                                    size="large"
-                                />
-                            </Form.Item>
-                            <Form.Item
-                                hasFeedback
-                                name="confirm"
-                            >
-                                <Input
-                                    type="password"
-                                    prefix={<LockOutlined className="site-form-item-icon" />}
-                                    placeholder="Повторить пароль"
-                                    size="large"
-                                />
-                            </Form.Item>
-                            <Form.Item>
-                                <Button type="primary" size="large" htmlType="submit">
-                                    ЗАРЕГИСТРИРОВАТЬСЯ
-                                </Button>
-                            </Form.Item>
-                            <Link className="auth__register-link" to="/">
-                                Войти в аккаунт
+                            <Input
+                                id="email"
+                                prefix={<MailOutlined className="site-form-item-icon" />}
+                                placeholder="E-mail"
+                                size="large"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="nickname"
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Ваше имя" size="large" />
+                        </Form.Item>
+                        <Form.Item
+                            hasFeedback
+                            name="password"
+                            validateStatus={
+                                !touched.normal_login_password ? "" : errors.password ? "error" : "success"
+                            }
+                            help={!touched.normal_login_password ? false : errors.password}
+                        >
+                            <Input
+                                id="password"
+                                type="password"
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                placeholder="Пароль"
+                                size="large"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            hasFeedback
+                            name="confirm"
+                            validateStatus={
+                                !touched.normal_login_confirm ? "" : errors.confirm ? "error" : "success"
+                            }
+                            help={!touched.normal_login_confirm ? false : errors.confirm}
+                        >
+                            <Input
+                                id="confirm"
+                                type="password"
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                placeholder="Повторить пароль"
+                                size="large"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" size="large" htmlType="submit" onClick={handleSubmit}>
+                                ЗАРЕГИСТРИРОВАТЬСЯ
+                            </Button>
+                        </Form.Item>
+                        <Link className="auth__register-link" to="/">
+                            Войти в аккаунт
                             </Link>
-                        </Form>
-                    ) : (
+                    </Form>
+                ) : (
                         <div className="auth__success-block">
                             <ExclamationCircleOutlined className="auth__success-icon" />
                             <h2>Подтвердите свой аккаунт</h2>
                             <p>На Вашу почту отправлено письмо с ссылкой на подтверждение аккаунта.</p>
                         </div>)
-                    }
-                </Block>
-            </>
-        );
-    }
+                }
+            </Block>
+        </>
+    )
 }
 
 export default RegisterForm;
--- src/modules/RegisterForm/containers/RegisterForm.js
+++ src/modules/RegisterForm/containers/RegisterForm.js
@@ -1,3 +1,40 @@
+import { withFormik } from 'formik';
 import RegisterForm from '../components/RegisterForm'
 
-export default RegisterForm
+export default withFormik({
+    validate: values => {
+        let errors = {};
+        if (!values.normal_login_email) {
+            errors.email = 'Введите email';
+        } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
+                values.normal_login_email
+            )
+        ) {
+            errors.email = 'Некорректный email';
+        }
+        
+        if (!values.normal_login_password) {
+            errors.password = 'Введите пароль'
+        } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/.test(values.normal_login_password)) {
+            errors.password = 'Слишком легкий пароль'
+        }
+
+        if (!values.normal_login_confirm) {
+            errors.confirm = 'Введите пароль еще раз'
+        } else if (values.normal_login_password !== values.normal_login_confirm) {
+            errors.confirm = 'Пароли не совпадают'
+        }
+
+        return errors;
+    },
+
+    handleSubmit: (values, { setSubmitting }) => {
+        setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+        }, 1000);
+    },
+
+    displayName: 'RegisterForm', // helps with React DevTools
+})(RegisterForm)
